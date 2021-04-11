74a7808494eb65e8b8a4df5d4accaea238170f25
Sonia133 <sorina.andrei2408@gmail.com>
2021-04-11 23:36:11 +0300

    fix errors

--- contracts/product.sol
+++ contracts/product.sol
@@ -49,6 +49,10 @@ contract Products
         return true;
     }
     
+    function CompareStrings(string memory _s1, string memory _s2) internal view returns(bool) {
+        return keccak256(abi.encodePacked(_s1)) == keccak256(abi.encodePacked(_s2));
+    }
+    
     function ReturnProduct(uint _index) internal view returns (string memory, string memory, string memory) {
         return (OProducts[_index].name, OProducts[_index].brand, OProducts[_index].country);
     }
@@ -58,11 +62,11 @@ contract Products
         int found = 0;
         // pentru ce nu vrem sa introducem in parametri vom pune ""
         
-        for (uint i = 0; i < IndexCountProduct - 1; i++) {
-            if (_name == "") or (_name == OProducts[i].name) {
-                if (_brand == "") or (_brand == OProducts[i].brand) {
-                    if (_country == "") or (_country == OProducts[i].country) {
-                        if (_unique_hash == "") or (_unique_hash == OProducts[i].unique_hash) {
+        for (uint i = 0; i < OProducts.length - 1; i++) {
+            if (CompareStrings(_name, "") || CompareStrings( _name, OProducts[i].name)) {
+                if (CompareStrings(_brand, "") || CompareStrings(_brand, OProducts[i].brand)) {
+                    if (CompareStrings(_country, "") || CompareStrings(_country, OProducts[i].country)) {
+                        if (CompareStrings(_unique_hash, "") || CompareStrings(_unique_hash, OProducts[i].unique_hash)) {
                             ReturnProduct(i);
                             found = 1;
                         }
@@ -80,8 +84,8 @@ contract Products
     
     function ReportProduct(string memory _unique_hash) public returns (bool) //Sorina
     {
-        for (uint i = 0; i < IndexCountProduct - 1; i++) {
-            if (_unique_hash == OProducts[i].unique_hash) {
+        for (uint i = 0; i < OProducts.length - 1; i++) {
+            if (CompareStrings(_unique_hash, OProducts[i].unique_hash)) {
                 return false;
             }
         }
