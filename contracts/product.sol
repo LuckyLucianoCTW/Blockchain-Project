28cbf70392b48e5e26d8e20925aafd8c0ef6153c
LuckyLuciano <luckylucianoredz@yahoo.com>
2021-04-11 12:31:06 +0300

    Finalize Structure

--- contracts/product.sol
+++ contracts/product.sol
@@ -3,19 +3,59 @@ pragma solidity ^0.5.0;
 In contractul pe care-l vom face vom avea urmatoarele elemente : 
 
 1) Structuri pentru produse
-2) Functii de : adaugare, stergere, interschimbare, cautare, report.
-
+2) Functii de : adaugare, stergere, update, cautare, report.
 */
 
 contract Products
 {
     address owner; //doar ownerul va avea dreptul de a adauga produse
-    
-    struct Prduct{
+    struct Product
+    {
         string name;
         string brand;
         string country;
         string unique_hash;
     }
     
+    int IndexCountProduct = 0;
+    Product [] OProducts;
+    
+    int IndexCountHash = 0;
+    string [] ReportedHashProducts;
+    
+    modifier OwnerReq() {
+        require(msg.sender == owner);
+        _;
+    }
+    
+    
+    constructor() public {
+        owner = msg.sender;
+    }
+
+    
+    function AddProduct() public OwnerReq returns (bool)
+    {
+        return true;
+    }
+    
+    function RemoveProduct() public OwnerReq returns (bool) 
+    {
+        return true;
+    }
+    
+    function UpdateProduct() public OwnerReq returns (bool) 
+    {
+        return true;
+    }
+    
+    function SearchProduct() public view returns (string memory,string memory,string memory,string memory) 
+    {
+        return (" "," "," "," ");
+    }
+    
+    function ReportProduct() public returns (bool) 
+    {
+        return true;
+    }
 }
